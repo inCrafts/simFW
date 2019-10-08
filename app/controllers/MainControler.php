c97591123bb935345532958899798a434bb457f8
inCrafts <inCrafts@mail.ru>
2019-10-08 22:04:30 +0300

    Структура фреймворка, основные файлы и классы

---
+++ app/controllers/MainControler.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\controllers;
+
+
+class MainControler {
+
+    public function indexAction () {
+        echo __METHOD__;
+    }
+}
---
+++ app/controllers/PageControler.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace app\controllers;
+
+
+class PageControler {
+
+    public function viewAction() {
+        echo __METHOD__;
+    }
+
+}
